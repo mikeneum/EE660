7d1ec4271656ae1007d2e1a63d2db5ffaa780e3b
mikeneum <mikeneum@usc.edu>
2015-12-07 13:22:30 -0800

    Create Stock_log_regression_lambda.m

---
+++ Stock_log_regression_lambda.m
@@ -0,0 +1,58 @@
+
+clear all
+load('StockData.mat')
+errRateMLE=zeros(30,1);
+err=zeros(12,1);
+j=0
+for i=1:12:360
+   j=1+j;
+X_train =X(i:i+11,:)
+y_train=y(i:i+11);
+%y_train=standardizeCols(y_train)
+X_train=standardizeCols(X_train);
+
+
+ 
+X_test=X(359+j+i:i+j+370, :)
+X_test=standardizeCols(X_test);
+y_test=y(359+j+i:i+j+370)
+%y_test=standardizeCols(y_test)
+
+
+
+
+
+% Log Regression
+lambda=7.8146;
+model = logregFit(X_train, y_train,'lambda',lambda );
+yhat = logregPredict(model, X_test)
+%errRateMLE(j) = mean(yhat ~= y_test);
+%Linear Regression
+%model = linregFit(X_train, y_train);
+%yhat = linregPredict(model, X_test)
+%delta=abs(yhat-y_test);
+for m=1:12
+if yhat(m)==y_test(m)
+    err(m)=0;
+else
+    err(m)=1;
+end
+end
+err
+errRateMLE(j)=sum(err)/12
+err=zeros(12,1);
+end
+mu=sum(errRateMLE)/30
+figure;
+bar(errRateMLE)
+hold on
+hline = refline([0 mu]);
+hline.Color = 'r';
+
+title('Mean error log regression, Lambda=7.8146')
+xlabel('Dow Jones Companies')
+%set(gca,'XTickLabel', {'AA','AXP','BA','BAC','CAT','CSCO','CVX',...
+  %  'DD','DIS','GE','HD','HPQ','IBM','INTC','JNJ','JPM',...
+   % 'KRFT','KO','MCD','MMM','MRK','MBFT','PPE','PG','T',...
+   % 'TRV','UTX','VZ','WMT','XOM'})
+ylabel('Mean Error')
